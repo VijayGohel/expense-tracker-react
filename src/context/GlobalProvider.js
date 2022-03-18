1e4c5730f14f0fb4c77be8b3fec569e198bd63ab
vijay <vijaygohel5454@gmail.com>
2022-03-18 22:20:32 +0530

    context api and transaction component

---
+++ src/context/GlobalProvider.js
@@ -0,0 +1,24 @@
+import { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer"
+const initialState = {
+    transactions:[
+        {id:1 ,text:"Flower", amount:-20},
+        {id:2 ,text:"Salary", amount:2000}
+    ]
+}
+
+export const GlobalContext =  createContext(initialState);
+
+const GlobalProvider = ({children})=>{
+
+    const [state, dispatch] = useReducer(AppReducer,initialState);
+
+    return(
+        <GlobalContext.Provider value={{
+            transactions: state.transactions
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+export default GlobalProvider;
