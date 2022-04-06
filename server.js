a5edf512bc416889959678c8954ffa63a9581966
vijay <vijaygohel5454@gmail.com>
2022-04-06 21:17:32 +0530

    heroku deployment

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/v1/transactions", transactions);
 
-if(process.env.NODE_ENV === "dev")
+if(process.env.NODE_ENV === "development")
 {
     app.use(morgan("dev"));
 }
-if(process.env.NODE_ENV === "prod")
+if(process.env.NODE_ENV === "production")
 {
     app.use(express.static("client/build"));
     app.get("*",(req,res)=>(res.sendFile(path.resolve(__dirname,"client","build","index.html"))));
